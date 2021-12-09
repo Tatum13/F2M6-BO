4750ab406a5ea279faa5274e3930692806588c50
Bas <30810@ma-web.nl>
2021-12-09 11:13:57 +0100

    Alle movement is nu okelie doklie

--- CupheadSlice/Assets/Scripts/Player/PlayerMovement.cs
+++ CupheadSlice/Assets/Scripts/Player/PlayerMovement.cs
@@ -51,27 +51,51 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) && isGrounded == true)
         {
             movement += Vector2.right * walkSpeed;
-            Debug.Log("rechts");
         }
         if (Input.GetKey(KeyCode.LeftArrow) && isGrounded == true)
         {
             movement += Vector2.left * walkSpeed;
-            Debug.Log("links");
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             movement += Vector2.right * walkSpeed;
-            Debug.Log("rechts");
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             movement += Vector2.left * walkSpeed;
-            Debug.Log("links");
         }
         rb2d.velocity = new Vector2(movement.x, rb2d.velocity.y);
     }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            StartCoroutine(JumpCoroutine());
+        }
+    }
+    IEnumerator JumpCoroutine()
+    {
+        isJumping = true;
+        jumpTimeCounter = jumpTime;
+
+        Rigidbody2D _rb = gameObject.GetComponent<Rigidbody2D>();
+        while (Input.GetButton("Jump"))
+        {
+            if (jumpTimeCounter > 0)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            yield return null;
+        }
+        isJumping = false;
+    }
     private void Dash1()
     {
+        if (isDashing == true)
+        {
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))//naar links dashen
         {
             if (doubbleTapTime > Time.time && lastKeyCode == KeyCode.LeftArrow)
@@ -98,41 +122,25 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
-        {
-            StartCoroutine(JumpCoroutine());
-        }
-    }
-    IEnumerator JumpCoroutine()
-    {
-        isJumping = true;
-        jumpTimeCounter = jumpTime;
-
-        Rigidbody2D _rb = gameObject.GetComponent<Rigidbody2D>();
-        while (Input.GetButton("Jump"))
-        {
-            if (jumpTimeCounter > 0)
-            {
-                _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
-                jumpTimeCounter -= Time.deltaTime;
-            }
-            yield return null;
-        }
-        isJumping = false;
-    }
     IEnumerator Dash2(float direction)
     {
         isDashing = true;
         Vector2 movement = Vector2.zero;
-        rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+        movement += new Vector2(dashDistance * direction, 0f);
+        
 
         /*rb2d.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);*/
-        movement += new Vector2(dashDistance * direction, 0f);
+        
 
         float gravity = rb2d.gravityScale;
         rb2d.gravityScale = 0;
+        float timer = 0.3f;
+        while(timer > 0)
+        {
+            rb2d.velocity = movement;
+            yield return null;
+            timer -= Time.deltaTime;
+        }
         yield return new WaitForSeconds(0.3f);
 
         rb2d.gravityScale = gravity;
